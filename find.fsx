1e965b2e615215ce1b61de0adcc72f1d557a1451
LazuliKao <LazuliKao233@outlook.com>
2025-05-10 15:15:52 +0800

    sync

--- find.fsx
+++ find.fsx
@@ -9,19 +9,28 @@ open System.Diagnostics
 let availableDnsServers =
     [
       // category: China DNS
-      //   "https://dns.alidns.com/dns-query"
-      //   "https://223.5.5.5/dns-query"
-      //   "https://223.6.6.6/dns-query"
-      //   "https://doh.pub/dns-query"
-      //   "https://1.12.12.12/dns-query"
-      //   "https://120.53.53.53/dns-query"
-      //   "https://doh.360.cn/dns-query"
-      //   "https://doh.apad.pro/dns-query"
+    //   "https://dns.alidns.com/dns-query"
+    //   "https://223.5.5.5/dns-query"
+    //   "https://223.6.6.6/dns-query"
+    //   "https://doh.pub/dns-query"
+    //   "https://1.12.12.12/dns-query"
+    //   "https://120.53.53.53/dns-query"
+    //   "https://doh.360.cn/dns-query"
+    //   "https://doh.apad.pro/dns-query"
       // category: Public DNS
+      "https://doh.sb/dns-query"
+      "https://doh-jp.blahdns.com/dns-query"
+      "https://doh.dns.sb/dns-query"
       "https://dns.google/dns-query"
-      "https://cloudflare-dns.com/dns-query"
       "https://jp.tiarap.org/dns-query"
       "https://dns.quad9.net/dns-query"
+      "https://dns10.quad9.net/dns-query"
+      "https://doh.cleanbrowsing.org/doh/security-filter/"
+      "https://doh.opendns.com/dns-query"
+      "https://cloudflare-dns.com/dns-query"
+      "https://dns-unfiltered.adguard.com/dns-query"
+      "https://1.1.1.1/dns-query"
+      "https://1.0.0.1/dns-query"
       "https://9.9.9.9/dns-query"
       "https://149.112.112.112/dns-query"
       "https://max.rethinkdns.com/dns-query"
@@ -50,7 +59,7 @@ let availableDnsServers =
 
 let query address dnsServer =
     task {
-        use httpClient = new HttpClient(Timeout = TimeSpan.FromSeconds 1.0)
+        use httpClient = new HttpClient(Timeout = TimeSpan.FromSeconds 20.0)
         httpClient.BaseAddress <- Uri dnsServer
         use dnsClient = new DnsHttpClient(httpClient) :> IDnsClient
         let query = DnsQueryFactory.CreateQuery address
